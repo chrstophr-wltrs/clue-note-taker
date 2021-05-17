402ba3aa143db9e44bfba7fbb60a95361b18dbac
Chris Walters <christopherman2cool@gmail.com>
2021-05-17 15:01:36 -0600

    implemented Player.pass()

--- scripts/clueNoteTaker.js
+++ scripts/clueNoteTaker.js
@@ -1,4 +1,9 @@
 const convert = {
+  // Key:
+  // 0 = Unknown
+  // 1 = Might Have
+  // 2 = Does Not Have
+  // 3 = Definitely Has
   0: ``,
   1: `<span class="maybe">?</span>`,
   2: `<span class="dontHave">X</span>`,
@@ -26,6 +31,20 @@ const convert = {
   study: "Study",
 };
 
+class Guess {
+  constructor(suspect, item, location) {
+    this.suspect = suspect;
+    this.item = item;
+    this.location = location;
+  }
+}
+
+Guess.prototype.toString = () => {
+  return `${convert[this.suspect]}/${convert[this.item]}/${
+    convert[this.location]
+  }`;
+};
+
 class Player {
   constructor(name, abbrev) {
     this.name = name;
@@ -36,7 +55,7 @@ class Player {
     // 2 = Does Not Have
     // 3 = Definitely Has
     this.notes = {
-      characters: {
+      suspects: {
         green: 0,
         mustard: 0,
         peacock: 0,
@@ -58,29 +77,39 @@ class Player {
       },
     };
     // Player.refutations will hold all of the instances where that Player was able to Refute a Guess
-    // Each item will be an array of strings
-    // For example, one item might be ["plum", "revolver", "library"]
-    // So you could have a Player.refutations of [["plum", "revolver", "library"], ["green", "knife", "hall"]] and so on
+    // Each item will be a Guess object
+    // For example, one item might be plum/revolver/library
+    // So you could have a Player.refutations of [plum/revolver/library, green/hall/knife] and so on
     this.refutations = [];
   }
-}
 
-class Guess {
-  constructor(character, item, location) {
-    this.character = character;
-    this.item = item;
-    this.location = location;
+  pass(guess) {
+    // Player cannot Refute a Guess, and therefore does not have any of the clues
+    // guess should always be an instance of a Guess object
+    this.notes.suspects[guess.suspect] = 2;
+    this.notes.items[guess.item] = 2;
+    this.notes.locations[guess.location] = 2;
+  }
+
+  refute(guess) {
+    // Player can Refute a Guess, but the user doesn't know which clue they have
+    // guess should always be an instance of a Guess object
+  }
+
+  trueRefute(guess) {
+    // Player can Refute a Guess, and Player reveals a clue to the user
+    // guess should always be an instance of a Guess object
+    const { suspect, item, location } = guess;
+    const { suspects, items, locations } = this.notes;
   }
 }
-
-Guess.prototype.toString = () => {
-  return `${convert[this.character]}/${convert[this.item]}/${
-    convert[this.location]
-  }`;
-};
 
 class NoteAssistant {
   constructor(players = ["Abigail", "Ben", "Cassandra", "David", "Emma"]) {
-    this.players;
+    this.players = [];
   }
+}
+
+function domLoaded() {
+  myNotes = new NoteAssistant();
 }
